d5b46b3765b4cc16063e41bb9a1a722908148bec
Anna-Anka <anjbekrenewa@gmail.com>
2022-04-21 10:22:00 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,43 @@
+$(function () {
+    $('.header__items').slick({
+        dots: false,
+        arrows: true,
+        prevArrow: '<button class="header__arrow header__arrow--prev"><svg class="header__icon"><use xlink:href="../img/sprite.svg#arrow-left-icon"></use></svg></button>',
+        nextArrow: '<button class="header__arrow header__arrow--next"><svg class="header__icon"><use xlink:href="../img/sprite.svg#arrow-right-icon"></use></svg></button>',
+        slidesToScroll: 2,
+        infinite: false,
+        slidesToShow: 2,
+    });
+
+    $('.main-slider__items').slick({
+        arrows: true,
+        dots: true,
+        prevArrow: '<button class="main-slider__arrow main-slider__arrow--prev"><svg class="main-slider__icon"><use xlink:href="../img/sprite.svg#arrow-left-icon"></use></svg></button>',
+        nextArrow: '<button class="main-slider__arrow main-slider__arrow--next"><svg class="main-slider__icon"><use xlink:href="../img/sprite.svg#arrow-right-icon"></use></svg></button>',
+        infinite: false,
+    })
+
+    //Счетчик
+    const number = document.querySelectorAll('.about-numbers__number');
+    number.forEach(item => {
+        window.addEventListener('scroll', function onScroll() {
+
+            var numberTop = item.getBoundingClientRect().top,
+                start = +item.innerHTML,
+                end = +item.dataset.max;
+
+            if (window.pageYOffset > numberTop - window.innerHeight / 2) {
+                this.removeEventListener('scroll', onScroll);
+                var interval = setInterval(function () {
+                    item.innerHTML = ++start;
+                    if (start == end) {
+                        clearInterval(interval);
+                    }
+                }, 0.00001);
+            }
+        });
+
+    });
+
+
+});
